9aa275c06a78a85213a8113e1687795881df1405
globulion <bartosz.blasiak@gmail.com>
2013-06-07 22:24:00 +0900

    Implementation of 6/8 R-4 terms in slvcor

--- solvshift/slvcor.py
+++ solvshift/slvcor.py
@@ -65,8 +65,7 @@ class SLVCOR(UNITS):
             
             for i in xrange(len(Ra)):
                 for j in xrange(len(Rb)):
-                    R    = Rb[j]-Ra[i]
-                    R   *= -1
+                    R    = Ra[i]-Rb[j]
                     Rab=sqrt(sum(R**2,axis=0))
                     
                     ### fi terms
@@ -79,9 +78,23 @@ class SLVCOR(UNITS):
                         rf3 +=   qa[i] * dot(self.__Lvec(I,i),Db[j])            / Rab**3
                         rf3 += 3*qb[j] * dot(self.__Lvec(I,i),R) * dot(Da[i],R) / Rab**5
                         rf3 -=   qb[j] * dot(self.__Lvec(I,i),Da[i])            / Rab**3
+                        #
+                        rf4 -= 5*qa[i] * dot(self.__Lvec(I,i),R) * dot(dot(Qb[j],R),R) / Rab**7
+                        rf4 += 2*qa[i] * dot(dot(Qb[j],self.__Lvec(I,i)),R)            / Rab**5
+                        #
+                        f1   =   dot(Da[i],R) * dot(self.__Lvec(I,i),Db[j])
+                        f1  +=   dot(self.__Lvec(I,i),R) * dot(Da[i],Db[j])
+                        f1  +=   dot(Db[j],R) * dot(self.__Lvec(I,i),Da[i])
+                        f1  *= -3/Rab**5
+                        rf4 += f1
+                        rf4 +=15*dot(Da[i],R) * dot(Db[j],R) * dot(self.__Lvec(I,i),R) / Rab**7
+                        #
+                        rf4 += 2*qb[j]*dot(dot(Qa[i],self.__Lvec(I,i)),R)              / Rab**5
+                        rf4 -= 5*qb[j]*dot(dot(Qa[i],R),R) * dot(self.__Lvec(I,i),R)   / Rab**7
                         
                         Rn_fi[I,1]+= rf2
                         Rn_fi[I,2]+= rf3
+                        Rn_fi[I,3]+= rf4
                         
                     ### kjj terms
                     rk2 = 0; rk3 = 0; rk4 = 0
@@ -95,9 +108,25 @@ class SLVCOR(UNITS):
                     rk3 -=    qa[i] * qb[j] * dot(self.__Lvec(J,i),self.__Lvec(J,i)) / Rab**3
                     rk3 += 6* qb[j] * dot(self.__Lvec(J,i),R) * dot(fDa[i],R)/ Rab**5
                     rk3 -= 2* qb[j] * dot(fDa[i],self.__Lvec(J,i))           / Rab**3
+                    #
+                    rk4 += 4*fqa[i] * dot(dot(Qb[j],self.__Lvec(J,i)),R)     / Rab**5
+                    rk4 -=10*fqa[i] * dot(self.__Lvec(J,i),R) * dot(dot(Qb[j],R),R) / Rab**7
+                    #
+                    k1   = 2*dot(self.__Lvec(J,i),R) * dot(self.__Lvec(J,i),Db[j])
+                    k1  +=   dot(self.__Lvec(J,i),self.__Lvec(J,i)) * dot(Db[j],R)
+                    k1  *=-3*qa[i]                                           / Rab**5
+                    rk4 += k1
+                    rk4 +=15*qa[i] * (dot(self.__Lvec(J,i),R))**2 * dot(Db[j],R) / Rab**7
+                    #
+                    k1   = 2*dot(self.__Lvec(J,i),R) * dot(self.__Lvec(J,i),Da[i])
+                    k1  +=   dot(self.__Lvec(J,i),self.__Lvec(J,i)) * dot(Da[i],R)
+                    k1  *= 3*qb[j]                                           / Rab**5
+                    rk4 += k1
+                    rk4 -=15*qb[j] * (dot(self.__Lvec(J,i),R))**2 * dot(Da[i],R) / Rab**7
                     
                     Rn_kjj[1] += rk2
                     Rn_kjj[2] += rk3
+                    Rn_kjj[3] += rk4
                     
             return Rn_fi, Rn_kjj
         
