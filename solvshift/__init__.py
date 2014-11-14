c3b47c11e6e0802dedcd7011be04f9f4d3e9186f
globulion <blasiak.bartosz@gmail.com>
2014-11-14 19:00:09 +0900

    Module file __init__.py added

--- solvshift/__init__.py
+++ solvshift/__init__.py
@@ -1,5 +1,5 @@
 ﻿# -*- coding: utf-8 -*-
 ### Happy module file
-import shftce, shftex, exrep, efprot
+import shftce, shftex, exrep, efprot, solpol, solpol2
 __author__=    "Bartosz Błasiak"
 __version__=   "1.0.1"
