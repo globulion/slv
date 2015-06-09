1f2f9be15d81ed41d08368bf841dd37e2651cea3
globulion <blasiak.bartosz@gmail.com>
2015-06-09 15:07:03 +0900

    Initialization of Rep-EA numerical evaluation

--- solvshift/src/exrep.f
+++ solvshift/src/exrep.f
@@ -1,3 +1,127 @@
+CC-----|--|---------|---------|---------|---------|---------|---------|--|------|
+CC
+CC      filename: shftex.f
+CC
+CC                EXCHANGE-REPULSION INTERACTION ENERGY THEORY
+CC                  ENERGIES FOR FD DISPLACEMENTS TO BE USED
+CC                IN NUMERICAL EVALUATION OF FREQUENCY SHIFTS
+CC
+CC                      version 0.0a    7 Jun 2015    Bartosz Błasiak
+CC                         ---- ODSTAWIONE NA PÓŹNIEJ ---- 
+CC -----------------------------------------------------------------------------
+CC
+C      SUBROUTINE EXREFD(RIA,RIB,RNA,RNB,FAIJ,FBIJ,CIKA,CIKB,
+C     &                  SKM,TKM,ZA,ZB,
+C     &                  NBSA,NBSB,NMOSA,NMOSB,NATA,NATB,NFD,NLMO,
+C     &                  NMOLS,NAT,NFOCK,NCB,NSKM,NTKM,
+C     &                  EINT)
+CC
+C      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+C      DIMENSION RIA(NFD*NMOSA*3),RIB(NLMO*3),RNA(NFD*NATA*3),RNB(NAT*3)
+C      DIMENSION NMOSB(NMOLS), NATB(NMOLS)
+C      DIMENSION FAIJ(NFD*NMOSA*NMOSA),FBIJ(NFOCK)
+C      DIMENSION CIKA(NFD*NMOSA*NBSA),CIKB(NCB),SKM(NSKM),TKM(NTKM)
+C      DIMENSION ZA(NATA),ZB(NAT),NBSB(NMOL),NMOSB(NMOL),NATB(NMOL)
+C      DIMENSION EINT(NFD),SIJ(*),TIJ(*)
+C      PARAMETER (ZERO=0.0D+00,ONE=1.0D+00,TWO=2.0D+00,THREE=3.0D+00,
+C     &           FOUR=4.0D+00,FIVE=5.0D+00)
+CCf2py INTENT(IN,OUT) EINT
+C      NBSTOT = 0
+C      NMOSTOT= 0
+C      DO N=1,NMOL
+C         NBSTOT = NBSTOT + NBSB(N)
+C         NMOSTOT= NMOSTOT+ NMOSB(N)
+C      ENDDO
+C      NBSTOT = NBSTOT * NBSA
+C      NMOSTOT= NMOSTOT* NMOSA
+CC
+CC     COMPUTE TAB AND SAB
+CC
+C      NBSBCUR = 0
+C      NMOSBCUR = 0
+C      DO NJ=1,NMOL
+C         IF (NJ.GT.1) THEN                                        
+C            NBSBCUR = NBSB(NJ-1) + NBSBCUR
+C            NMOSBCUR= NMOSB(NJ-1) + NMOSBCUR
+C         ENDIF
+C         DO J=1,NMOS(NJ)
+C            DO NF=1,NFD
+C               DO I=1,NMOSA
+C                  NFIJ = NMOSTOT*(NF-1) + NMOSBCUR*(I-1) + J
+C                  VALS = ZERO
+C                  VALT = ZERO
+C                  DO II=1,NBSA
+C                     NCIKA = NBSA*NMOSA*(NF-1) + NBSA*(I-1) + II
+C                     DO JJ=1,NBSB(NJ)                                
+C                        NCIKB = NBSBCUR*(J-1) + JJ
+C                        NSKM = NBSTOT*(NF-1) + NBSBCUR * (II-1) + JJ
+C                        CC = CIKA(NCIKA) * CIKB(NCIKB)
+C                        VALS = VALS + CC * SKM(NSKM)
+C                        VALT = VALT + CC * TKM(NSKM)
+C                     ENDDO
+C                  ENDDO
+C                  SIJ(NFIJ) = VALS
+C                  TIJ(NFIJ) = VALT
+C               ENDDO
+C            ENDDO
+C         ENDDO
+C      ENDDO
+CC
+CC     ITERATE OVER ALL SOLVENT MOLECULES
+CC
+C      NMOSBCUR = 0
+C      DO NJ=1,NMOL
+C         IF (NJ.GT.1) THEN                    
+C             NMOSBCUR= NMOSB(NJ-1) + NMOSBCUR
+C         ENDIF
+C      DO J=1,NMOS(NJ)
+C         NRJX = NMOSBCUR*3 + 3*(J-1) + 1
+C         RJX = RIB(NRJX  )
+C         RJY = RIB(NRJX+1)
+C         RJZ = RIB(NRJX+2)
+CC
+CC        ITERATE OVER FD DISPLACEMENTES
+CC
+C         DO NF=1,NFD
+CC
+CC           ITERATE OVER IR MOLECULE LMOs
+CC
+C            VE = ZERO
+C            DO I=1,NMOSA
+C               NRIX = NMOSA*3*(NF-1) + 3*(I-1) + 1
+C               RIX = RIA(NRIX  )
+C               RIY = RIA(NRIX+1)
+C               RIZ = RIA(NRIX+2)
+C               RIJX= RIX - RJX
+C               RIJY= RIY - RJY
+C               RIJZ= RIZ - RJZ
+C               RIJV = DSQRT(RIJX*RIJX+RIJY*RIJY+RIJZ*RIJZ)
+C               RIJV = ONE/RIJV
+CC
+C               TERM1 = ZERO
+C               TERM2 = ZERO
+CC
+C               NFIJ = NMOSTOT*(NF-1) + NMOSBCUR*(I-1) + J
+CC
+C               SIJV = SIJ(NFIJ)
+C               TIJV = TIJ(NFIJ)
+C               AAA = DSQRT(-TWO*DLOG(DABS(SIJV)))
+CC
+C               TERM1 = -TWO * TIJV
+C               TERM2 = - RIJV
+CC
+C               VE = VE - FOUR * AAA * SIJV * SIJV * RIJV
+C               VE = VE - TWO * SIJV * TERM1
+C               VE = VE + TWO * SIJV * SIJV * TERM2
+C            ENDDO
+C            EINT(NF) = EINT(NF) + VE
+CC
+C         ENDDO
+CC
+C      ENDDO
+C      ENDDO
+CC
+C      RETURN
 C-----|--|---------|---------|---------|---------|---------|---------|--|------|
 C
 C      filename: shftex.f
